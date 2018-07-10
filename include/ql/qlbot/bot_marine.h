be2df8a452780bab9b16c3fb73e3ce5c643851bd
branopal <36081491+branopal@users.noreply.github.com>
2018-07-10 22:00:50 +0200

    bot_marine - pridanie QL
    uprava bota, pridanie QL, pridanie feature

--- include/ql/qlbot/bot_marine.h
+++ include/ql/qlbot/bot_marine.h
@@ -1,9 +1,27 @@
 #pragma once
 #include "sc2api/sc2_agent.h"
 
-class MarineBot : public sc2::Agent {
-public:
+#include "marine_feature.h"
+#include "qllib/Stav.h"
+#include "qllib/QL.h"
+
+class MarineBot : public sc2::Agent
+{
+    int tagPrveho;
     uint32_t restarts_;
+    MarineFeature* mfeature_;
+    QL* ql_;
+    Stav* state_;
+
+    float reward;
+    float global_reward;
+    int lastAction;
+    int step;
+    int poslednySelectnuty;
+
+    void GetState() const;
+    void SaveQL();
+public:
     MarineBot();
     void OnGameStart() override final;
     void OnStep() override final;
